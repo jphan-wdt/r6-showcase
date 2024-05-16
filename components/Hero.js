9cea3369813dfbae03853adb09150371bd892295
ec221013 <ec221013@qmul.ac.uk>
2024-05-16 17:19:57 +0100

    Hero core finished

--- components/Hero.js
+++ components/Hero.js
@@ -7,18 +7,21 @@ export default function Hero() {
         <div className={styles.textContainer}>
             <div className={styles.menu}>
                 <button className={styles.menuItem}>Welcome</button>
-                <button className={styles.menuItem}>Menu 1</button>
-                <button className={styles.menuItem}>Menu 2</button>
-                <button className={styles.menuItem}>Menu 3 </button>
-                <button className={styles.menuItem}>Menu 4</button>
-                <button className={styles.menuItem}>Menu 5</button>
+                <button className={styles.menuItem}>Learn the Basics</button>
+                <button className={styles.menuItem}>Meet Team Rainbow</button>
+                <button className={styles.menuItem}>Master Gadgetry</button>
+                <button className={styles.menuItem}>Embrace Destruction</button>
+                <button className={styles.menuItem}>Travel the World</button>
             </div>
             <div className={styles.textRight}>
-                <div className={styles.title}>Title</div>
-                <br></br>
-                <div className={styles.description}>Description</div>
+                <div className={styles.title}>Strategic Precisionists</div>
+                <br/>
+                <br/>
+                <div className={styles.description}>
+                    Prepare, strategise, eliminate the most
+                    formidable opponents in the world of Rainbow Six
+                </div>
             </div>
-
         </div>
 
         <VideoPlayer/>
